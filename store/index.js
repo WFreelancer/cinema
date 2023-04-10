d862d607163e8aad6f4926ce97dfdd0997109c52
bsgdigital <markkostenko94@gmail.com>
2023-04-10 14:01:32 -0400

    sd

--- store/index.js
+++ store/index.js
@@ -7,19 +7,13 @@ import storage from 'redux-persist/lib/storage';
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['search']
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-// not to production
-let composeEnhancers;
-if (typeof window !== 'undefined') {
-  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-}
+
 // not to production
 export const store = createStore(
 	persistedReducer,
-	composeEnhancers // < not to production
 );
 
 export const persistor = persistStore(store);
